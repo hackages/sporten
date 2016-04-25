2df3c4fbcdbbafa8498552ba622ff577a1ab32c3
davyengone <engonenzedavy@yahoo.com>
2016-04-25 21:10:22 +0200

    the app will be available on sportenweb.surge.sh

---
+++ packages/web/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Router, Route, hashHistory, browserHistory } from 'react-router';
+import ProfilePage from './components/profile';
+import EventsPage from './components/events';
+import NoMatchPage from './components/noMatch';
+import App from './components/app';
+import EventDetailsPage from './components/event';
+
+const Main = () => {
+  return (
+    <Router history={ hashHistory }>
+       <Route path="/" component={ App }/>
+       <Route path="/profile" component={ ProfilePage }/>
+       <Route path="/events" component={ EventsPage }/>
+       <Route path="/event" component={ EventDetailsPage }/>
+       <Route path="*" component={ NoMatchPage }/>
+    </Router>
+  );
+};
+
+ReactDom.render(<Main/>, document.getElementById('sporten'));
+
+export default App;
