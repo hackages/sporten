022c4b41289e9a7de9d83e5c8ea79a3c760562b3
davyengone <engonenzedavy@yahoo.com>
2016-04-29 21:24:33 +0200

    added splash page

--- packages/mobile/index.ios.js
+++ packages/mobile/index.ios.js
@@ -1,9 +1,8 @@
 /**
- * Sample React Native App
+ * Sporten built with React Native
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, {
   AppRegistry,
   Component,
@@ -11,43 +10,17 @@ import React, {
   Text,
   View
 } from 'react-native';
-import NavigationBar from 'react-native-navbar';
-import Navigation from './components/navigation';
 
-const Nav = () => {
- return (<View style={{ flex: 1, backgroundColor: '#ff9900', }}>
-         <NavigationBar
-        title={{ title: 'Title', }}
-        leftButton={{ title: 'Back', }}
-        rightButton={{ title: 'Forward', }} />
-      </View>);
-};
+import App from './components/';
 
 class sporten extends Component {
   render() {
     return (
-      <Navigation/>
-      );
+      <View>
+       <App/>
+      </View>
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('sporten', () => sporten);
