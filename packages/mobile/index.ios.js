050e8c9a8f320a795165361fec54d2c8273f9582
davyengone <engonenzedavy@yahoo.com>
2016-05-01 21:12:52 +0200

    added realm

--- packages/mobile/index.ios.js
+++ packages/mobile/index.ios.js
@@ -8,17 +8,66 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS,
+  TouchableHighlight,
 } from 'react-native';
 
 import App from './components/';
 import Profile from './components/profile';
+import Navigation from './components/navigation';
+// import Events from './components/events.js';
+import Maps from './components/maps';
+import ListItem from './components/listItem';
+import ListView from './components/listview';
+import LV from './components/lv';
+import Realm from 'realm';
+
+const Event = {
+  tile: 'Basketball On Sunday',
+  max: 15,
+  price: 4,
+  attendees: []
+};
+
+const User = {
+  name: 'Davy Engone',
+  email: 'davy@hackages.io',
+  num: 7,
+  sports: ['Basket', 'Tenis']
+};
+
+const Nav = () => {
+  return (
+    <NavigatorIOS
+      initialRoute={{
+        component: Profile,
+        title: 'Events',
+        passProps: { myProp: 'foo' },
+      }}
+    />
+  );
+};
+
+
 
 class sporten extends Component {
-  render() {
+  join () {
+    Event.attendees = [User.email, ...Event.attendees];
+    console.log(Event);
+  }
+
+  cancel () {
+
+  }
+
+  render () {
     return (
       <View>
-       <Profile/>
+       <Navigation/>
+       <TouchableHighlight onPress={this.join.bind(this)}>
+        <Text>Join</Text>
+       </TouchableHighlight>
       </View>
     );
   }
