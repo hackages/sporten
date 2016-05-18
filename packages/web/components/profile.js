375b1ea98b55d034a51aeeeba275cc421523e59d
davyengone <engonenzedavy@yahoo.com>
2016-05-18 23:09:54 +0200

    proto of profile

--- packages/web/components/profile.js
+++ packages/web/components/profile.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import 'material-design-lite/material.js';
+import 'material-design-lite/material.css';
+import '../css/app.css';
 
-const Profile = () => <h1>Profile page</h1>;
+class Profile extends Component {
+ render () {
+   return (
+     <div>
+     <header>
+       <div className='mdl-js-ripple-effect'>
+        <img className='profile' src='../img/dave.jpg'/>
+       </div>
+     </header>
+     <div className='sub-header'>
+     </div>
+     </div>
+   );
+ }
+}
 
 export default Profile;
