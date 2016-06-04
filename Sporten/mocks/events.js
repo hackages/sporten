119ab78854fbe153334f56ba0b2d8c3afd80575d
davyengone <engonenzedavy@yahoo.com>
2016-06-04 21:50:35 +0200

    mock data

---
+++ Sporten/mocks/events.js
@@ -0,0 +1,15 @@
+export default events = [
+  {
+    title: 'BaBaBru Basketball',
+    organizers: ['davyengone'],
+    date: 'Sunday 19 June 2016',
+    where: 'Sporthal Diependal, 3080, Tervuren',
+    max: 15,
+    min: 10,
+    cost: [2.5, 4],
+    attendees: [],
+    waitinglist: [],
+    private: true,
+    totalcost: 32,
+  },
+];
---
+++ Sporten/mocks/users.js
@@ -0,0 +1,20 @@
+export default users = [
+  {
+    username: 'davyengone',
+    fullname: 'davy engone nze',
+    sport: 'Basketball',
+    picture: 'images/davy.png',
+  },
+  {
+    username: 'marciosampiao',
+    fullname: 'Marcio Sampaio',
+    sport: 'Basketball',
+    picture: 'images/marcio.png',
+  },
+  {
+    username: 'whoc',
+    fullname: 'Wil Hoc',
+    sport: 'Basketball',
+    picture: 'images/wil.png',
+  },
+]
